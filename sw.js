c7c8bf7b03a400708eea05c5360e861c6ed8080a
fischerpj <fischer.pj@gmail.com>
2026-09-26 16:02:16 +0000

    2640

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'quarto-pwa-v1';
 const ASSETS_TO_CACHE = [
-  './index.html',
-//  './bcv_parser.js',
-//  './refengine.js',
-//  './refui_sole.js',
-//  './lang/en.js',
-//  './manifest.json'
+  '/resvlt/posts/refpwa.html',
+  '/resvlt/posts/bcv_parser.js',
+  '/resvlt/posts/refengine.js',
+  '/resvlt/posts/refui_sole.js',
+  '/resvlt/posts/lang/en.js',
+  '/resvlt/posts/manifest.json'
 ];
 
 // Install Event: Cache core files
